736601e6f9157c7ddf05f8a85875de661443176b
Hai-Yue Han <hahan@sunpowercorp.com>
2020-02-18 19:51:54 -0800

    OneDrive ate my files; here's what I have been able to recover. At least it compiles!

---
+++ Current Cycling/TDK Lambda Communications/TDK Lambda Communications/TDK Lambda Communications/frmMain.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace TDK_Lambda_Communications
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serTDK = new System.IO.Ports.SerialPort(this.components);
+            this.btnEnumerate = new System.Windows.Forms.Button();
+            this.cmbSerial = new System.Windows.Forms.ComboBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblInstVoltage = new System.Windows.Forms.Label();
+            this.lblInstCurrent = new System.Windows.Forms.Label();
+            this.btnSetAddress = new System.Windows.Forms.Button();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtVoltage = new System.Windows.Forms.TextBox();
+            this.btnSetCurrent = new System.Windows.Forms.Button();
+            this.txtCurrent = new System.Windows.Forms.TextBox();
+            this.btnSetVoltage = new System.Windows.Forms.Button();
+            this.btnOff = new System.Windows.Forms.Button();
+            this.btnOn = new System.Windows.Forms.Button();
+            this.tmrMain = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnEnumerate
+            // 
+            this.btnEnumerate.Location = new System.Drawing.Point(17, 65);
+            this.btnEnumerate.Name = "btnEnumerate";
+            this.btnEnumerate.Size = new System.Drawing.Size(121, 61);
+            this.btnEnumerate.TabIndex = 0;
+            this.btnEnumerate.Text = "Enumerate Serial Ports";
+            this.btnEnumerate.UseVisualStyleBackColor = true;
+            this.btnEnumerate.Click += new System.EventHandler(this.btnEnumerate_Click);
+            // 
+            // cmbSerial
+            // 
+            this.cmbSerial.FormattingEnabled = true;
+            this.cmbSerial.Location = new System.Drawing.Point(17, 12);
+            this.cmbSerial.Name = "cmbSerial";
+            this.cmbSerial.Size = new System.Drawing.Size(121, 28);
+            this.cmbSerial.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(17, 141);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(121, 38);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open Port";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Enabled = false;
+            this.btnClose.Location = new System.Drawing.Point(17, 185);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(121, 38);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close Port";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblInstVoltage
+            // 
+            this.lblInstVoltage.AutoSize = true;
+            this.lblInstVoltage.Location = new System.Drawing.Point(13, 276);
+            this.lblInstVoltage.Name = "lblInstVoltage";
+            this.lblInstVoltage.Size = new System.Drawing.Size(68, 20);
+            this.lblInstVoltage.TabIndex = 4;
+            this.lblInstVoltage.Text = "Voltage:";
+            // 
+            // lblInstCurrent
+            // 
+            this.lblInstCurrent.AutoSize = true;
+            this.lblInstCurrent.Location = new System.Drawing.Point(13, 312);
+            this.lblInstCurrent.Name = "lblInstCurrent";
+            this.lblInstCurrent.Size = new System.Drawing.Size(70, 20);
+            this.lblInstCurrent.TabIndex = 5;
+            this.lblInstCurrent.Text = "Current: ";
+            // 
+            // btnSetAddress
+            // 
+            this.btnSetAddress.Location = new System.Drawing.Point(175, 12);
+            this.btnSetAddress.Name = "btnSetAddress";
+            this.btnSetAddress.Size = new System.Drawing.Size(121, 38);
+            this.btnSetAddress.TabIndex = 6;
+            this.btnSetAddress.Text = "Set Address";
+            this.btnSetAddress.UseVisualStyleBackColor = true;
+            this.btnSetAddress.Click += new System.EventHandler(this.btnSetAddress_Click);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(314, 18);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(63, 26);
+            this.txtAddress.TabIndex = 7;
+            this.txtAddress.Text = "1";
+            // 
+            // txtVoltage
+            // 
+            this.txtVoltage.Location = new System.Drawing.Point(314, 147);
+            this.txtVoltage.Name = "txtVoltage";
+            this.txtVoltage.Size = new System.Drawing.Size(63, 26);
+            this.txtVoltage.TabIndex = 9;
+            this.txtVoltage.Text = "0";
+            // 
+            // btnSetCurrent
+            // 
+            this.btnSetCurrent.Location = new System.Drawing.Point(175, 185);
+            this.btnSetCurrent.Name = "btnSetCurrent";
+            this.btnSetCurrent.Size = new System.Drawing.Size(121, 38);
+            this.btnSetCurrent.TabIndex = 8;
+            this.btnSetCurrent.Text = "Set Current";
+            this.btnSetCurrent.UseVisualStyleBackColor = true;
+            this.btnSetCurrent.Click += new System.EventHandler(this.btnSetCurrent_Click);
+            // 
+            // txtCurrent
+            // 
+            this.txtCurrent.Location = new System.Drawing.Point(314, 191);
+            this.txtCurrent.Name = "txtCurrent";
+            this.txtCurrent.Size = new System.Drawing.Size(63, 26);
+            this.txtCurrent.TabIndex = 11;
+            this.txtCurrent.Text = "0";
+            // 
+            // btnSetVoltage
+            // 
+            this.btnSetVoltage.Location = new System.Drawing.Point(175, 141);
+            this.btnSetVoltage.Name = "btnSetVoltage";
+            this.btnSetVoltage.Size = new System.Drawing.Size(121, 38);
+            this.btnSetVoltage.TabIndex = 10;
+            this.btnSetVoltage.Text = "Set Voltage";
+            this.btnSetVoltage.UseVisualStyleBackColor = true;
+            this.btnSetVoltage.Click += new System.EventHandler(this.btnSetVoltage_Click);
+            // 
+            // btnOff
+            // 
+            this.btnOff.Location = new System.Drawing.Point(423, 185);
+            this.btnOff.Name = "btnOff";
+            this.btnOff.Size = new System.Drawing.Size(121, 38);
+            this.btnOff.TabIndex = 13;
+            this.btnOff.Text = "Off";
+            this.btnOff.UseVisualStyleBackColor = true;
+            this.btnOff.Click += new System.EventHandler(this.btnOff_Click);
+            // 
+            // btnOn
+            // 
+            this.btnOn.Location = new System.Drawing.Point(423, 141);
+            this.btnOn.Name = "btnOn";
+            this.btnOn.Size = new System.Drawing.Size(121, 38);
+            this.btnOn.TabIndex = 12;
+            this.btnOn.Text = "On";
+            this.btnOn.UseVisualStyleBackColor = true;
+            this.btnOn.Click += new System.EventHandler(this.btnOn_Click);
+            // 
+            // tmrMain
+            // 
+            this.tmrMain.Tick += new System.EventHandler(this.tmrMain_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 406);
+            this.Controls.Add(this.btnOff);
+            this.Controls.Add(this.btnOn);
+            this.Controls.Add(this.txtCurrent);
+            this.Controls.Add(this.btnSetVoltage);
+            this.Controls.Add(this.txtVoltage);
+            this.Controls.Add(this.btnSetCurrent);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.btnSetAddress);
+            this.Controls.Add(this.lblInstCurrent);
+            this.Controls.Add(this.lblInstVoltage);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.cmbSerial);
+            this.Controls.Add(this.btnEnumerate);
+            this.Name = "frmMain";
+            this.Text = "TDK Power Supply Communication Sample";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serTDK;
+        private System.Windows.Forms.Button btnEnumerate;
+        private System.Windows.Forms.ComboBox cmbSerial;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblInstVoltage;
+        private System.Windows.Forms.Label lblInstCurrent;
+        private System.Windows.Forms.Button btnSetAddress;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtVoltage;
+        private System.Windows.Forms.Button btnSetCurrent;
+        private System.Windows.Forms.TextBox txtCurrent;
+        private System.Windows.Forms.Button btnSetVoltage;
+        private System.Windows.Forms.Button btnOff;
+        private System.Windows.Forms.Button btnOn;
+        private System.Windows.Forms.Timer tmrMain;
+    }
+}
+
---
+++ Current Cycling/TDK Lambda Communications/TDK Lambda Communications/frmMain.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace TDK_Lambda_Communications
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnumerate_Click(object sender, EventArgs e)
+        {
+            //Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+
+            //Clears the combo box
+            cmbSerial.Items.Clear();
+
+            //Adds each port name to the combo box
+            foreach (string port in ports)
+            {
+                cmbSerial.Items.Add(port);
+            }
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            //Sets the serial port up and then opens the port
+            serTDK.BaudRate = 57600;
+            serTDK.PortName = cmbSerial.Text;
+            serTDK.NewLine = "\r";
+            serTDK.ReadTimeout = 1000;
+            serTDK.Open();
+            
+            //Sets the GUI correctly
+            btnClose.Enabled = true;
+            btnOpen.Enabled = false;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            //Closes the port
+            serTDK.Close();
+
+            //Reenables the open button and disables the close button
+            btnOpen.Enabled = true;
+            btnClose.Enabled = false;
+        }
+
+        //Sets the address of the TDK Lambda
+        private Boolean setAddress (string strAddress)
+        {
+            //Sets the address of the power supply
+            serTDK.Write("ADR " + strAddress + "\r\n");
+
+            //Reads the response and see if we got an error or not
+            if (serTDK.ReadLine() == "OK")
+            {
+                //Return success
+                return true;
+            }
+            else
+            {
+                //Returns failure
+                return false;
+            }
+        }
+
+
+
+        //Sets the voltage of the TDK Lambda
+        private Boolean setVoltage(string strVoltage)
+        {
+            //Sets the voltage of the power supply
+            serTDK.Write("PV " + strVoltage + "\r\n");
+
+            //Reads the response and see if we got an error or not
+            if (serTDK.ReadLine() == "OK")
+            {
+                //Return success
+                return true;
+            }
+            else
+            {
+                //Returns failure
+                return false;
+            }
+        }
+
+        //Sets the current of the TDK Lambda
+        private Boolean setCurrent(string strCurrent)
+        {
+            //Sets the current limit of the power supply
+            serTDK.Write("PC " + strCurrent + "\r\n");
+
+            //Reads the response and see if we got an error or not
+            if (serTDK.ReadLine() == "OK")
+            {
+                //Return success
+                return true;
+            }
+            else
+            {
+                //Returns failure
+                return false;
+            }
+        }
+
+        //Sets the output state of the TDK Lambda
+        private Boolean setOutput(Boolean bolOn)
+        {
+            //Checks to see if we want to turn it on or off
+            if (bolOn == true)
+            {
+                //Sets the power supply to be on
+                serTDK.Write("OUT 1\r\n");
+            }
+            else
+            {
+                //Sets the power supply to be off
+                serTDK.Write("OUT 0\r\n");
+            }
+
+            //Reads the response and see if we got an error or not
+            if (serTDK.ReadLine() == "OK")
+            {
+                //Return success
+                return true;
+            }
+            else
+            {
+                //Returns failure
+                return false;
+            }
+        }
+
+        //Gets the instantaneous voltage of the power supply
+        private string getVoltage()
+        {
+            //Sets the current limit of the power supply
+            serTDK.Write("MV?\r\n");
+
+            //Reads the response and returns it
+            return serTDK.ReadLine();     
+        }
+
+        //Gets the instantaneous current of the power supply
+        private string getCurrent()
+        {
+            //Sets the current limit of the power supply
+            serTDK.Write("MC?\r\n");
+
+            //Reads the response and returns it
+            return serTDK.ReadLine();
+        }
+
+        private void btnSetAddress_Click(object sender, EventArgs e)
+        {
+            setAddress(txtAddress.Text);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSetCurrent_Click(object sender, EventArgs e)
+        {
+            setCurrent(txtCurrent.Text);
+
+        }
+
+        private void btnSetVoltage_Click(object sender, EventArgs e)
+        {
+            setVoltage(txtVoltage.Text);
+        }
+
+        private void btnOn_Click(object sender, EventArgs e)
+        {
+            //Turns the power supply on and starts reading voltage and current
+            setOutput(true);
+            tmrMain.Enabled = true;
+        }
+
+        private void btnOff_Click(object sender, EventArgs e)
+        {
+            //Turns the power supply off and disables voltage/current reading
+            setOutput(false);
+            tmrMain.Enabled = false;
+        }
+
+        private void tmrMain_Tick(object sender, EventArgs e)
+        {
+            //Reads the voltage and current from the power supply
+            lblInstVoltage.Text = "Voltage: " + getVoltage();
+            lblInstCurrent.Text = "Current: " + getCurrent();
+        }
+    }
+}
