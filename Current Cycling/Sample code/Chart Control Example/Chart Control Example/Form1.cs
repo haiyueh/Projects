0e0a9aee02f60435d943bef5b1eaaf792d087b4d
Hai-Yue Han <hahan@sunpowercorp.com>
2020-03-27 11:24:59 -0700

    Added chart control sample code

---
+++ Current Cycling/Sample code/Chart Control Example/Chart Control Example/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chart_Control_Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chtMain.Series["Voltage by Cycle"].Points.AddXY(1, 3.4);
+            chtMain.Series["Voltage by Cycle"].Points.AddXY(2, 4.4);
+            chtMain.Series["Voltage by Cycle"].Points.AddXY(3, 1.4);
+            chtMain.Series["Voltage by Cycle"].Points.AddXY(4, 3.4);
+            chtMain.Series["Voltage by Cycle"].Points.AddXY(5, 5.4);
+            chtMain.Series["Voltage by Cycle"].Points.AddXY(6, 7.4);
+            chtMain.Series["Voltage by Cycle"].Points.AddXY(7, 3.9);
+            chtMain.Series["Voltage by Cycle"].Points.AddXY(8, 8.4);
+            chtMain.SaveImage("C:\\temp\\whohoo.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+
+        private void chtMain_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
