d5898d9bb16950452e90c0075a90111d875c5498
Hai-Yue Han <hahan@sunpowercorp.com>
2020-09-03 12:12:21 -0700

    Able to read all channels and print to serial

--- Leakage Detect/Arduino/LeakageDetect/LeakageDetect.ino
+++ Leakage Detect/Arduino/LeakageDetect/LeakageDetect.ino
@@ -29,30 +29,30 @@ int intADCPin[8][2] = {
 };
 
 //ADC values
-int intADCValues[16][4];
+int intADCValues[70];
 
 //ADC programmable gain amplifier
-int intADCPGA[16] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+int intADCPGA[64] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
 
 //ADC programmable gain amplifier constants
 uint16_t intPGADefinition[6] = {GAIN_TWOTHIRDS, GAIN_ONE, GAIN_TWO, GAIN_FOUR, GAIN_EIGHT, GAIN_SIXTEEN};
 
 
-
-
 //====================================================
-//Function: setADCGain
-//Input: ads - instance of the Adafruit ADC
-//       intADCPGAIndex - index of the ADC (1 to 16)
+//Function: setup
+//Input: none
+//Output: none
 //====================================================
-void setADCGain(Adafruit_ADS1115 ads, int intADCPGAIndex){
-  ads.setGain(intPGADefinition[intADCPGAIndex]
-}
-
 void setup(void) {
-  
+  //DEBUG
+  Serial.begin(115200);
 }
 
+//====================================================
+//Function: loop
+//Input: none
+//Output: none
+//====================================================
 void loop(void){
   //Declarations
   int intBoardIndex = 0;
@@ -61,62 +61,47 @@ void loop(void){
   for (intBoardIndex = 0; intBoardIndex < NUMBER_OF_BOARDS; intBoardIndex++){
     //Establishes connection to ADC for channels 1-4
     Adafruit_ADS1115 ads1(ADC_ADDRESS_CHANNELS_1_TO_4,intADCPin[intBoardIndex][0],intADCPin[intBoardIndex][1]); 
+    ads1.begin();
 
-    //Sets the gain for the ADC
-    ads1.setGain(intPGADefinition[intADCPGA[intBoardIndex*2]]);
 
-    //Stores the ADC values
-    intADCValues[intBoardIndex*2][0] = ads1.readADC_SingleEnded(0);
-    intADCValues[intBoardIndex*2][1] = ads1.readADC_SingleEnded(1);
-    intADCValues[intBoardIndex*2][2] = ads1.readADC_SingleEnded(2);
-    intADCValues[intBoardIndex*2][3] = ads1.readADC_SingleEnded(3);
+    //Sets the gain and then reads the ADC values
+    ads1.setGain(intPGADefinition[intADCPGA[intBoardIndex*8+0]]);
+    intADCValues[intBoardIndex*8+0] = ads1.readADC_SingleEnded(0);
     
+    ads1.setGain(intPGADefinition[intADCPGA[intBoardIndex*8+1]]);
+    intADCValues[intBoardIndex*8+1] = ads1.readADC_SingleEnded(1);
     
+    ads1.setGain(intPGADefinition[intADCPGA[intBoardIndex*8+2]]);
+    intADCValues[intBoardIndex*8+2] = ads1.readADC_SingleEnded(2);
     
+    ads1.setGain(intPGADefinition[intADCPGA[intBoardIndex*8+3]]);
+    intADCValues[intBoardIndex*8+3] = ads1.readADC_SingleEnded(3);
+
+    
+
+    //Establishes connection to ADC for channels 5-8
+    Adafruit_ADS1115 ads2(ADC_ADDRESS_CHANNELS_5_TO_8,intADCPin[intBoardIndex][0],intADCPin[intBoardIndex][1]); 
+    ads2.begin();
+
+    ads2.setGain(intPGADefinition[intADCPGA[intBoardIndex*8+4]]);
+    intADCValues[intBoardIndex*8+4] = ads2.readADC_SingleEnded(0);
+    
+    ads2.setGain(intPGADefinition[intADCPGA[intBoardIndex*8+5]]);
+    intADCValues[intBoardIndex*8+5] = ads2.readADC_SingleEnded(1);
+    
+    ads2.setGain(intPGADefinition[intADCPGA[intBoardIndex*8+6]]);
+    intADCValues[intBoardIndex*8+6] = ads2.readADC_SingleEnded(2);
+    
+    ads2.setGain(intPGADefinition[intADCPGA[intBoardIndex*8+7]]);
+    intADCValues[intBoardIndex*8+7] = ads2.readADC_SingleEnded(3);
   }
+
+  //DEBUG
+  for (int i = 0; i < 64; i++){
+    Serial.print(intADCValues[i]);
+    Serial.print(",");
+  }
+  Serial.println();
+  //delay(100);
   
 }
-
-
-
-//CLK,DA
-//Adafruit_ADS1115 ads(0x4a,intADCPin[5][0],intADCPin[5][1]);  /* Use this for the 16-bit version */
-
-
-//void setup(void) 
-//{
-//  Serial.begin(9600);
-//  Serial.println("Hello!");
-//  
-//  Serial.println("Getting single-ended readings from AIN0..3");
-//  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
-//  
-//  // The ADC input range (or gain) can be changed via the following
-//  // functions, but be careful never to exceed VDD +0.3V max, or to
-//  // exceed the upper and lower limits if you adjust the input range!
-//  // Setting these values incorrectly may destroy your ADC!
-//  //                                                                ADS1015  ADS1115
-//  //                                                                -------  -------
-//  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
-//  ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
-//  ads.begin();
-//}
-//
-//void loop(void) 
-//{
-//  //Est
-//
-//  
-//  int16_t adc0, adc1, adc2, adc3;
-//  adc0 = ads.readADC_SingleEnded(0);
-//  adc1 = ads.readADC_SingleEnded(1);
-//  adc2 = ads.readADC_SingleEnded(2);
-//  adc3 = ads.readADC_SingleEnded(3);
-//  Serial.print("AIN0: "); Serial.println(adc0);
-//  Serial.print("AIN1: "); Serial.println(adc1);
-//  Serial.print("AIN2: "); Serial.println(adc2);
-//  Serial.print("AIN3: "); Serial.println(adc3);
-//  Serial.println(" ");
-//  delay(1000);
-//  
-//}
